84ed98186130d2687b7be09835f3a539a231dae6
JCriz01 <96634701+JCriz01@users.noreply.github.com>
2025-03-03 13:24:09 -0500

    Integrate TanStack Router

--- apps/prism-webapp/src/main.tsx
+++ apps/prism-webapp/src/main.tsx
@@ -1,16 +1,24 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { ReactDOM } from "react";
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router";
-import App from "./App.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
 
-// This is the entry point of the application. The App component is rendered
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+})
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('app')!
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(<RouterProvider router={router} />)
+}
